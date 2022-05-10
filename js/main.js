a96a78e81a90b0de9332adbcb78f2b2a2d6e1f34
Thyago França <thyagofranch@gmail.com>
2022-05-10 16:35:12 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const btnIcon = document.getElementById('btn-icon');
-const navMenu = document.getElementById('nav-menu');
-const openMenu = () => {
-	btnIcon.classList.toggle('fa-times');
-	navMenu.classList.toggle('active');
-};
-btnIcon.addEventListener('click', openMenu);
